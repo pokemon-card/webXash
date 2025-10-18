da644935bad2580a8c14708b40a9d6b0d20640f6
pokemon-card <136928586+pokemon-card@users.noreply.github.com>
2025-10-17 21:46:19 -0400

    Update games.ts

--- src/config/games.ts
+++ src/config/games.ts
@@ -9,7 +9,7 @@ const zipList = [
   },
   {
     name: "Half-Life",
-    packageName: "hl.zip",
+    packageName: "https://drive.usercontent.google.com/download?id=1nbOqP4d6c-fDCAxNLiObA29_U-49Nc88&confirm=t&uuid=a691e3e6-5814-4598-8d66-884b9ca14ea4&at=AKSUxGMMaWtkT_1OA6JWUln4COqj%3A1760751900098",
   },
   {
     name: "Day One (78M)",
